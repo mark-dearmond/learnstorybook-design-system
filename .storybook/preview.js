ae2916f1c44276e88128a6cb6d2d530b2178478b
Mark DeArmond <bobb.dearmond@gmail.com>
2022-02-02 15:28:32 -0600

    added docs

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -16,12 +16,17 @@ export const decorators = [
   ),
 ];
 
+/*
+ * More on Storybook global parameters at:
+ * https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
+ */
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
+  // Storybook a11y addon configuration
+  a11y: {
+    // the target DOM element
+    element: "#root",
+    // sets the execution mode for the addon
+    manual: false,
   },
 };
